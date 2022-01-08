22ad740d547246756d9f9837fe1595d386bc880d
Raasin Amin Rahman <77879314+RaasinRahman@users.noreply.github.com>
2022-01-08 00:35:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,53 +2,54 @@ import random
 import pygame, sys
 from pygame.math import Vector2
 
+#Creating a snake object
 class Snake:
     def __init__(self):
 
-        self.body = [Vector2(5, 10), Vector2(4, 10), Vector2(3, 10)]
-        self.direction = Vector2(0, 0)
-        self.new_block = False
-        self.crunch_sound = pygame.mixer.Sound('attributes/crunch.wav')
+        self.body = [Vector2(5, 10), Vector2(4, 10), Vector2(3, 10)] #Creating a 2d object
+        self.direction = Vector2(0, 0) #Setting the starting direction of the snake
+        self.new_block = False #New block will start as False and become True whenever the snake eats an apple
+        self.crunch_sound = pygame.mixer.Sound('attributes/crunch.wav') #Setting the sound for when the snake eats an apple
 
     def spawn(self):
-        for block in self.body:
+        for block in self.body: #Drawing the snake onto the canvase using rects in pygame
             x = int(block.x * cell_size)
             y = int(block.y * cell_size)
             block_rect = pygame.Rect(x,y, cell_size, cell_size)
             pygame.draw.rect(screen, (141, 61, 255), block_rect)
 
     def move(self):
-        if self.new_block == True:
+        if self.new_block == True: #If new_block is True a new block/rect will be added to the back of the snake
             body_copy = self.body[:]
             body_copy.insert(0, body_copy[0] + self.direction)
             self.body = body_copy[:]
             self.new_block = False
         else:
-            body_copy = self.body[:-1]
+            body_copy = self.body[:-1] #Otherwise the blocks within the snake will follow the head
             body_copy.insert(0, body_copy[0] + self.direction)
             self.body = body_copy[:]
 
-    def grow(self):
+    def grow(self): #Setting new_block to True, adding a block to the back of the snake.
         self.new_block = True
 
-    def crunch(self):
+    def crunch(self): #When crunch is called it will play the crunch sound that is imported
         self.crunch_sound.play()
 
-    def die(self):
+    def die(self): #When die is called the snake will go back to it's orignal postion and size
         self.body = [Vector2(5, 10), Vector2(4, 10), Vector2(3, 10)]
         self.direction = Vector2(0, 0)
 
-
+#Creating a fruit object
 class Apple:
     def __init__(self):
         self.random()
 
-    def random(self):
+    def random(self): #sets a random co-ordinate for each time the apple will spawn
         self.x = random.randint(0, cell_number - 1)
         self.y = random.randint(0, cell_number - 1)
         self.pos = Vector2(self.x, self.y)
 
-    def spawn_fruit(self):
+    def spawn_fruit(self): #will spawn a rect within the given co-oridnates
         fruit_rect = pygame.Rect(int(self.pos.x * cell_size), int(self.pos.y * cell_size), cell_size, cell_size)
         screen.blit(apple, fruit_rect)
 
@@ -57,18 +58,18 @@ class MAIN:
     def __init__(self):
         self.snake = Snake()
         self.fruit = Apple()
-
-    def update(self):
+    
+    def update(self): #updates the game will moving frames and checking for cases in which the game will restart and be over.
         self.snake.move()
         self.hit()
         self.check_over()
 
-    def display(self):
+    def display(self): #draws the elements onto the canvas
         self.fruit.spawn_fruit()
         self.snake.spawn()
         self.display_score()
 
-    def check_over(self):
+    def check_over(self): #checks if the game will be over in the case of which the snake hits itself or the wall
         if not 0 <= self.snake.body[0].x <= cell_number or not 0 <= self.snake.body[0].y < cell_number:
             self.snake.die()
 
@@ -76,14 +77,14 @@ class MAIN:
             if block == self.snake.body[0]:
                 self.snake.die()
 
-    def hit(self):
+    def hit(self): #checks to see if the snake will hit the apple and then given the function: grow, it will grow
         if self.fruit.pos == self.snake.body[0]:
             self.fruit.random()
             self.snake.grow()
             self.snake.crunch()
 
 
-    def display_score(self):
+    def display_score(self): #Finds the current score and draws it onto the canvas
         score_text = str(len(self.snake.body) - 3)
         score_surface = game_font.render(score_text,True, (0, 0, 0))
         score_x = int(cell_size * cell_number - 50)
@@ -93,7 +94,7 @@ class MAIN:
 
 
 
-    def game_over(self):
+    def game_over(self): #quit the game
         pygame.quit()
         sys.exit()
 
@@ -110,6 +111,7 @@ pygame.time.set_timer(SCREEN_UPDATE, 150)
 main_game = MAIN()
 screen.fill((175, 215, 70))
 
+#creating the menu screen
 menu = True
 while menu:
     for event in pygame.event.get():
@@ -142,7 +144,8 @@ while run:
             sys.exit()
         if event.type == SCREEN_UPDATE:
             main_game.update()
-
+            
+        #user input ->
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
                 if main_game.snake.direction.y != 1:
